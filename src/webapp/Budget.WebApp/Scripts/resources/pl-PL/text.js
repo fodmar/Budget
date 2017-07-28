b413dbcdf5262f5c2409c8d16b1579e6b3213540
fodmar <marcinfoder@gmail.com>
2017-07-28 18:13:56 +0200

    install resxtojson tool

---
+++ src/webapp/Budget.WebApp/Scripts/resources/pl-PL/text.js
@@ -0,0 +1,7 @@
+﻿define({
+  "Password": "Hasło",
+  "SignIn": "Zaloguj się",
+  "ThisFieldIsRequired": "To pole jest wymagane",
+  "Username": "Nazwa użytkownika",
+  "UsernameAndPasswordDontMatch": "Nazwa użytkownika i hasło nie pasują"
+});
---
+++ src/webapp/Budget.WebApp/Scripts/resources/text.js
@@ -0,0 +1,10 @@
+﻿define({
+  "root": {
+    "Password": "Password",
+    "SignIn": "Sign in",
+    "ThisFieldIsRequired": "This field is required",
+    "Username": "Username",
+    "UsernameAndPasswordDontMatch": "Username and password don't match"
+  },
+  "pl-PL": true
+});
