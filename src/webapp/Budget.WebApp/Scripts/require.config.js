392816be69715d54577134a826a3ccfcb03cae4d
fodmar <marcinfoder@gmail.com>
2017-08-01 21:10:53 +0200

    install require.js

---
+++ src/webapp/Budget.WebApp/Scripts/require.config.js
@@ -0,0 +1,18 @@
+﻿requirejs.config({
+    baseUrl: '/Scripts',
+    paths: {
+        app: 'app',
+        lib: 'lib',
+        jquery: 'lib/jquery-3.1.1',
+        jqueryValidate: 'lib/jquery.validate',
+        jqueryValidateUnobtrusive: 'lib/jquery.validate.unobtrusive'
+    },
+    shim: {
+        jqueryValidate: {
+            deps: ['jquery']
+        },
+        jqueryValidateUnobtrusive: {
+            deps: ['jqueryValidate']
+        }
+    }
+});
---
+++ src/webapp/Budget.WebApp/Utils/RequireJs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Budget.WebApp.Utils
+{
+    public static class RequireJs
+    {
+        public static IHtmlString Require(string path)
+        {
+            string scriptsPath = VirtualPathUtility.ToAbsolute("~/Scripts/");
+
+            string script = string.Format(
+                @"<script src='/Scripts/require.js'></script>
+                  <script>
+                    require(['{0}require.config.js'], function () {{
+                         require(['{1}']);
+                    }});
+                  </script>", scriptsPath, path);
+
+            return new HtmlString(script);
+        }
+    }
+}
