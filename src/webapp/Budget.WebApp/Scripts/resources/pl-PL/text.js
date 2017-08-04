6b4b6dbc1c32c2d52e58692da4506bbb09bda7cd
fodmar <marcinfoder@gmail.com>
2017-08-04 21:21:09 +0200

    clean up

--- src/webapp/Budget.WebApp/Scripts/resources/pl-PL/text.js
+++
@@ -1,8 +0,0 @@
-﻿define({
-  "LogIntoApplication": "Zaloguj się do aplikacji",
-  "Password": "Hasło",
-  "SignIn": "Zaloguj się",
-  "ThisFieldIsRequired": "To pole jest wymagane",
-  "Username": "Nazwa użytkownika",
-  "UsernameAndPasswordDontMatch": "Nazwa użytkownika i hasło nie pasują"
-});
--- src/webapp/Budget.WebApp/Utils/RequireJs.cs
+++ src/webapp/Budget.WebApp/Utils/RequireJs.cs
@@ -10,12 +10,12 @@ namespace Budget.WebApp.Utils
     {
         public static IHtmlString Require(string path)
         {
-            string scriptsPath = VirtualPathUtility.ToAbsolute("~/Scripts/");
+            string scriptsPath = VirtualPathUtility.ToAbsolute("~/Scripts");
 
             string script = string.Format(
-                @"<script src='/Scripts/lib/require.js'></script>
+                @"<script src='{0}/lib/require.js'></script>
                   <script>
-                    require(['{0}require.config.js'], function () {{
+                    require(['{0}/require.config.js'], function () {{
                          require(['{1}'], function (module) {{
                              module.init();
                          }});
