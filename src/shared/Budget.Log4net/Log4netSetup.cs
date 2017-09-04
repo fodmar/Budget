0328177b6322ff2fdfd9184d327dca3155e01a1b
fodmar <marcinfoder@gmail.com>
2017-09-04 19:59:44 +0200

    configure log4net

---
+++ src/shared/Budget.Log4net/Log4netSetup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Config;
+
+namespace Budget.Log4net
+{
+    public static class Log4netSetup
+    {
+        public static void Setup()
+        {
+            Setup("log4net.config");
+        }
+
+        public static void Setup(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException(string.Format("File not found: {0} current dir: {1}", fileName, Directory.GetCurrentDirectory()));
+            }
+
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(fileName));
+        }
+    }
+}
---
+++ tests/integration/Budget.IntegrationTests.Log4net/Log4netTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budget.Log4net;
+using NUnit.Framework;
+
+namespace Budget.IntegrationTests.Log4net
+{
+    [TestFixture]
+    class Log4netTests
+    {
+        [Test]
+        [Ignore]
+        public void LogSomething()
+        {
+            Log4netSetup.Setup();
+            LoggerWrapper log = new LoggerWrapper();
+
+            try
+            {
+                this.Exception();
+            }
+            catch (Exception ex)
+            {
+                log.Error("Test exception.", ex);
+            }
+        }
+
+        private void Exception()
+        {
+            object o = null;
+            o.ToString();
+        }
+    }
+}
