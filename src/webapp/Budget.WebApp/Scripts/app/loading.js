4b29ef6d1eefede1a92b5d467ada43f7a6c98da4
fodmar <marcinfoder@gmail.com>
2017-09-14 21:10:34 +0200

    loading function for wrapping ajax calls

---
+++ src/webapp/Budget.WebApp/Scripts/app/loading.js
@@ -0,0 +1,17 @@
+﻿define(['jquery'], function () {
+
+    function loading(callback) {
+        return function () {
+            var target = $(this);
+            target.addClass("loading");
+
+            $.when(callback.apply(this, arguments)).always(function () {
+                target.removeClass("loading");
+            });
+        };
+    };
+
+    return {
+        loading: loading
+    };
+});
