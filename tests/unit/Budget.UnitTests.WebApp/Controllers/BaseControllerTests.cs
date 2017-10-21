7c1e1c5add6b092787d28b2cd76597d74ca4ccdc
fodmar <marcinfoder@gmail.com>
2017-10-21 13:34:32 +0200

    controller integrity test

---
+++ tests/unit/Budget.UnitTests.WebApp/Controllers/BaseControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budget.WebApp.Controllers;
+using NUnit.Framework;
+
+namespace Budget.UnitTests.WebApp.Controllers
+{
+    [TestFixture]
+    class BaseControllerTests
+    {
+        [Test]
+        public void AllControllersShouldDeriveFromBaseController()
+        {
+            Type baseControllerType = typeof(BaseController);
+            var controllers = baseControllerType
+                .Assembly
+                .GetTypes()
+                .Where(t => !t.FullName.Contains("T4MVC") &&
+                            t.Name.EndsWith("Controller") &&
+                            !baseControllerType.IsAssignableFrom(t));
+
+            CollectionAssert.IsEmpty(controllers, string.Join(" ", controllers.Select(c => c.FullName)));
+        }
+    }
+}
